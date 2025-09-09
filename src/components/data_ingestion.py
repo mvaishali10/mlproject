38b50ce5b7cb71132a889a75d5a728233b36e932
mvaishali10 <vaishalimamidi10@gmail.com>
2025-09-09 07:38:52 +0530

    data transformation done

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -6,8 +6,12 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
 
+from src.components.data_transformation import DataTransformation
+from src.components.data_transformation import DataTransformationConfig
+
 @dataclass
 class DataIngestionConfig:
+    # empty strings
     train_data_path: str=os.path.join('artifacts',"train.csv")
     test_data_path: str=os.path.join('artifacts',"test.csv")
     raw_data_path: str=os.path.join('artifacts',"raw.csv")
@@ -19,11 +23,14 @@ class DataIngestion:
     def initiate_data_ingestion(self):
         logging.info("Enterted the data ingestion method or component")
         try:
+            # loading dataset into df
             df=pd.read_csv(r"D:\mlproject1\mlproject\notebook\data\stud.csv")
             logging.info("Read the dataset as dataframe")
 
+            # making dir with name artifacts
             os.makedirs(os.path.dirname(self.ingestion_config.train_data_path),exist_ok=True)
 
+            # writing the data into the raw data
             df.to_csv(self.ingestion_config.raw_data_path,index=False,header=True)
 
             logging.info("Train test split initiated")
@@ -36,6 +43,7 @@ class DataIngestion:
             logging.info("Ingestion of the data is completed")
 
             return(
+                # returning the paths since it is neede for data transformation
                 self.ingestion_config.train_data_path,
                 self.ingestion_config.test_data_path
             )
@@ -44,4 +52,7 @@ class DataIngestion:
         
 if __name__=="__main__":
     obj=DataIngestion()
-    obj.initiate_data_ingestion()
+    train_data,test_data=obj.initiate_data_ingestion()
+
+    data_transformation=DataTransformation()
+    train_arr,test_arr,_=data_transformation.initiate_data_transformation(train_data,test_data)
